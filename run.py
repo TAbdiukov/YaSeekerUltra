e3a37b5e176b579f43b8760b053f24dd4aa41dd7
Tim Abdiukov <TAbdiukov@gmail.com>
2026-03-25 05:28:02 +0700

    Bugfixes for: Py3.14, fix relative import

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 #! /usr/bin/env python3
 from yaseeker import cli
 
-if __name__ == "__main__":
-    cli.run()
+def run():
+    if sys.version_info >= (3, 7):
+        asyncio.run(main())
+    else:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        try:
+            loop.run_until_complete(main())
+        finally:
+            loop.close()
