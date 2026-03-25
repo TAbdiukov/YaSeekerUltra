6778bb06a1231af20d674e098e1d392fa02e5f20
Tim Abdiukov <TAbdiukov@gmail.com>
2026-03-25 07:28:06 +0700

    Lint; Update User-Agent

--- yaseeker/core.py
+++ yaseeker/core.py
@@ -14,7 +14,7 @@ from aiohttp import TCPConnector, ClientSession
 from .executor import AsyncioProgressbarQueueExecutor, AsyncioSimpleExecutor
 
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36',
 }
 
 COOKIES_FILENAME = 'cookies.txt'
